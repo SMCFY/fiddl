263745a7e30b41e156572c7b3eee88b90db442f9
gericsapo <gericsapo@gmail.com>
2017-09-27 23:18:46 +0200

    AudioSampleBuffer added

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,18 +1,6 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
 class MainContentComponent   : public AudioAppComponent
 {
 public:
@@ -22,7 +10,7 @@ public:
         setSize (800, 600);
 
         // specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        //setAudioChannels (2, 2);
     }
 
     ~MainContentComponent()
@@ -38,28 +26,43 @@ public:
 
         // You can use this function to initialise any resources you might need,
         // but be careful - it will be called on the audio thread, not the GUI thread.
-
-        // For more details, see the help for AudioProcessor::prepareToPlay()
     }
 
     void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
     {
-        // Your audio-processing code goes here!
 
-        // For more details, see the help for AudioProcessor::getNextAudioBlock()
+        // I/O channels acquired from sample buffer and the framebuffer
+        const int numInputChannels = sampBuff.getNumChannels();
+        const int numOutputChannels = bufferToFill.buffer->getNumChannels();
+
+        for (int ch = 0; ch < numOutputChannels; ch++) // iterate through output channels
+        {
+            // reading from the sample buffer to the framebuffer
+            bufferToFill.buffer->copyFrom(
+                ch, // destination channel
+                writeIndex, // destination sample
+                sampBuff, // source buffer
+                ch % numInputChannels, // source channel
+                readIndex // source sample
+                samplesToProcess); // number of samples to process
+        }
+
+
+
+
 
         // Right now we are not producing any data, in which case we need to clear the buffer
         // (to prevent the output of random noise)
-        bufferToFill.clearActiveBufferRegion();
+        //bufferToFill.clearActiveBufferRegion();
     }
 
     void releaseResources() override
     {
         // This will be called when the audio device stops, or when it is being
         // restarted due to a setting change.
-
-        // For more details, see the help for AudioProcessor::releaseResources()
-    }
+        
+        sampBuff.setSize (0, 0); // deallocate memory by resizing the buffer to 0x0
+    }   
 
     //==============================================================================
     void paint (Graphics& g) override
@@ -82,7 +85,9 @@ private:
     //==============================================================================
 
     // Your private member variables go here...
-
+    AudioSampleBuffer sampBuff;
+    
+    
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
 };
