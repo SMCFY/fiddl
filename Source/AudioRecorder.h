6c061af03baa1b5c720a2759c76956bc47d205f3
Michael Castanieto <michaelcastanieto@h347.aau1x.klient.sydhavn.site.aau.dk>
2017-09-27 16:05:59 +0200

    added AudioRecorder.h header file

---
+++ Source/AudioRecorder.h
@@ -0,0 +1,45 @@
+/*
+  ==============================================================================
+
+    AudioRecorder.h
+    Created: 27 Sep 2017 1:30:17pm
+    Author:  Michael Castanieto
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class AudioRecorder : public AudioIODeviceCallback
+{
+    public:
+        // constructer
+        AudioRecorder();
+        // destructor
+        ~AudioRecorder();
+        
+        // public methods
+        void startRecording (const File& file);
+        void stop ();
+        bool isRecording () const;
+        void audioDeviceAboutToStart (AudioIODevice* device) override;
+        void audioDeviceStopped () override;
+        void audioDeviceIOCallback (const float** inputChannelData, int numInputChannels,
+                                    float** outputChannelData, int numOutputChannels,
+                                    int numSamples) override;
+        
+    private:  
+        // private variables
+        AudioThumbnail& thumbnail;
+        TimeSliceThread backgroundThread; // the thread that will write audio data to disk
+        ScopedPointer<AudioFormatWriter::ThreadedWriter> threadedWriter; // the FIFO used to buffer the incoming data
+        double sampleRate;
+        int64 nextSampleNum;
+        
+        CriticalSection writerLock;
+        AudioFormatWriter::ThreadedWriter* volatile activeWriter;
+    
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioRecorder);
+};
