c2cf128a833bde5b89eac6e828bb9e0aa79926d1
Michael Castanieto <michaelcastanieto@h135.aau1x.klient.sydhavn.site.aau.dk>
2017-10-23 14:40:17 +0200

    added a Gain class to test DSP functionality

---
+++ Source/Gain.cpp
@@ -0,0 +1,43 @@
+/*
+  ==============================================================================
+
+    Gain.cpp
+    Created: 23 Oct 2017 12:36:57pm
+    Authors: Michael Castanieto
+             Gergely Csapo
+             Jonas Holfelt
+
+  ==============================================================================
+*/
+
+#include "Gain.h"
+
+Gain::Gain(AudioParameterFloat &gain)
+{
+    // initialise the DSP state array, something like this:
+    /*
+    state = new float*[numChannels];
+    for (int ch = 0; ch < numChannels; ch++)
+    {
+        state[ch] = new float[bufferLengthInSamples];
+    }
+    */
+    this->gain = gain;
+}
+
+Gain::~Gain()
+{
+    // delete the state array, something like this:
+    /*
+    for (int i = 0; i < numChannels; i++)
+    {
+        delete state[i];
+    }
+    delete state;
+    */
+}
+
+void Gain::process(AudioBuffer<float> buffer)
+{
+    /* set the gain level here */
+}
---
+++ Source/Gain.h
@@ -0,0 +1,32 @@
+/*
+  ==============================================================================
+
+    Gain.h
+    Created: 23 Oct 2017 12:36:57pm
+    Authors: Michael Castanieto
+             Gergely Csapo
+             Jonas Holfelt
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "DSP.h"
+
+class Gain : public DSP
+{
+	public:
+		Gain(AudioParameterFloat *gain);
+		~Gain();
+
+		void process(AudioBuffer<float> buffer) override;
+
+	private:
+		/* parameter variables */
+		/* look at the AudioProcessorParameter class documentation
+		   for more info on how to use these parameters */
+		AudioParameterFloat* gain;
+
+		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Gain);
+};
