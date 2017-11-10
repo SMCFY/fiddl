4c5186e6e26fe97b3ac8550b536448ce758538f4
Michael Castanieto <mcastanieto@gmail.com>
2017-11-10 22:39:06 +0100

    new fileeees

---
+++ Source/ToggleSpaceComponent.cpp
@@ -0,0 +1,49 @@
+/*
+  ==============================================================================
+
+    ToggleSpaceComponent.cpp
+    Created: 10 Nov 2017 8:37:18pm
+    Author:  Michael Castanieto
+
+  ==============================================================================
+*/
+
+#include "../../../../../FiguraTK/JuceLibraryCode/JuceHeader.h"
+#include "ToggleSpaceComponent.h"
+
+//==============================================================================
+ToggleSpaceComponent::ToggleSpaceComponent()
+{
+    addAndMakeVisible (toggleSustain);
+    toggleSustain.setButtonText ("Sustain");
+    //toggleSustain.addListener (this);
+    
+    addAndMakeVisible (toggleImpulse);
+    toggleImpulse.setButtonText ("Impulse");
+    //toggleImpulse.addListener (this);
+
+}
+
+ToggleSpaceComponent::~ToggleSpaceComponent()
+{
+}
+
+void ToggleSpaceComponent::paint (Graphics& g)
+{
+    /* This demo code just fills the component's background and
+       draws some placeholder text to get you started.
+
+       You should replace everything in this method with your own
+       drawing code..
+    */
+
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));   // clear the background
+    //g.setColour (Colours::grey);
+    //g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
+}
+
+void ToggleSpaceComponent::resized()
+{
+    toggleSustain.setBounds(getWidth()-25, 10-getHeight()/2, 50,50);
+    toggleImpulse.setBounds(getWidth()-25, 30-getHeight()/2, 50,50);
+}
---
+++ Source/ToggleSpaceComponent.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    ToggleSpaceComponent.h
+    Created: 10 Nov 2017 8:37:18pm
+    Author:  Michael Castanieto
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../../../../../FiguraTK/JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+*/
+class ToggleSpaceComponent    : public Component
+{
+public:
+    ToggleSpaceComponent();
+    ~ToggleSpaceComponent();
+    
+    void paint (Graphics&) override;
+    void resized() override;
+    //void buttonStateChanged (Button* button) override;
+    //void buttonClicked (Button* button) override;
+
+private:
+    ToggleButton toggleSustain;
+    ToggleButton toggleImpulse;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ToggleSpaceComponent)
+};
